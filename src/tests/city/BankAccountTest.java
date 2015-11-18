62427bfce78335e19ac06be76d245e73e4e8aa38
Aymeric Giraudet <neadj59@gmail.com>
2015-11-18 19:15:49 +0100

    BankAccountTest

---
+++ src/tests/city/BankAccountTest.java
@@ -0,0 +1,43 @@
+package tests.city;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import city.BankAccount;
+
+public class BankAccountTest {
+
+	@Test
+	public void testIsInstantiated() {
+		BankAccount testedBankAccount = new BankAccount(0);
+		assertNotNull(testedBankAccount);
+		assertEquals(0, testedBankAccount.getTotal());
+	}
+
+	@Test
+	public void testCredit() {
+		BankAccount testedBankAccount = new BankAccount(0);
+		testedBankAccount.credit(10);
+		assertEquals(10, testedBankAccount.getTotal());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreditException() {
+		BankAccount testedBankAccount = new BankAccount(0);
+		testedBankAccount.credit(-10);
+	}
+	
+	@Test
+	public void testDebit() {
+		BankAccount testedBankAccount = new BankAccount(10);
+		testedBankAccount.debit(10);
+		assertEquals(0, testedBankAccount.getTotal());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testDebitException() {
+		BankAccount testedBankAccount = new BankAccount(10);
+		testedBankAccount.debit(-10);
+	}
+}
