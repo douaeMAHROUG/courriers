765469912ec6209a92d9a46604b723d3ca605732
douae.mahroug <douae.mahroug@etudiant.univ-lille1.fr>
2015-11-20 00:27:45 +0100

    doc de content.java

--- src/content/Content.java
+++ src/content/Content.java
@@ -3,6 +3,8 @@ package content;
  * 
  * @author mahroug
  *
+ * This interface contains a method that describes the type of the content of a letter
+ * We have implemented two types: MoneyContent and TextContent
  */
 
 public interface Content {
